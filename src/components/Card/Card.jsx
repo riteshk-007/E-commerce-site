03d47ff7ce2783f9cb680c663bd477a71b9ab116
Ritesh <codeshorts007@gmail.com>
2023-08-02 18:33:56 +0530

    design home page product cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable react/prop-types */
 import "./Card.scss";
+import { Link } from "react-router-dom";
+import { BsCurrencyRupee } from "react-icons/bs";
 
-function Card() {
-  return <div>card</div>;
+function Card({ item }) {
+  return (
+    <Link to={`/product/${item.id}`} className="link">
+      <div className="card">
+        <div className="image">
+          {item.isNew && <span>New Season</span>}
+          <img src={item.img} alt="" className="mainImage" />
+          <img src={item.img2} alt="" className="secondImage" />
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3>
+            <BsCurrencyRupee />
+            {item.oldPrice}
+          </h3>
+          <h3>
+            <BsCurrencyRupee />
+            {item.price}
+          </h3>
+        </div>
+      </div>
+    </Link>
+  );
 }
 
 export default Card;
